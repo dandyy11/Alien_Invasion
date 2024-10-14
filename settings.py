2cca138f6c428087114e103dd7fe433ba3b24987
dandyy <salman_imtiaz1@baylor.edu>
2024-10-13 22:38:00 -0500

    Final changes with fixes

--- settings.py
+++ settings.py
@@ -3,45 +3,45 @@ class Settings:
 
     def __init__(self):
         """Initialize the game's static settings."""
-        # Screen settings
+        
         self.screen_width = 1280
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
 
-        # Ship settings
-        self.ship_speed = 0.4  # Slowed ship speed
-        self.ship_limit = 3  # Number of lives
+        
+        self.ship_speed = 0.4  
+        self.ship_limit = 3  
 
-        # Bullet settings
-        self.bullet_speed = 0.5  # Slowed bullet speed
+       
+        self.bullet_speed = 0.5  
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 3
 
-        # Alien settings
-        self.alien_speed = 0.3  # Slowed alien speed
+        
+        self.alien_speed = 0.3  
         self.fleet_drop_speed = 10
-        self.fleet_direction = 1  # 1 represents right, -1 represents left
+        self.fleet_direction = 1  
 
-        # How quickly the game speeds up
+       
         self.speedup_scale = 1.1
 
-        # How quickly the alien point values increase
+       
         self.score_scale = 1.5
 
         self.initialize_dynamic_settings()
 
     def initialize_dynamic_settings(self):
         """Initialize settings that change throughout the game."""
-        self.ship_speed = 0.4  # Reduced ship speed
-        self.bullet_speed = 0.5  # Reduced bullet speed
-        self.alien_speed = 0.3  # Reduced alien speed
+        self.ship_speed = 0.4  
+        self.bullet_speed = 0.5  
+        self.alien_speed = 0.3  
 
-        # Fleet direction of 1 represents right; -1 represents left.
+        
         self.fleet_direction = 1
 
-        # Scoring
+        
         self.alien_points = 50
 
     def increase_speed(self):
--- ship.py
+++ ship.py
@@ -11,7 +11,7 @@ class Ship(Sprite):
         self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
-        # Load the ship image and get its rect.
+       
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
 
